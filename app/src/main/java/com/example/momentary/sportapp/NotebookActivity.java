e9a900d30e634b44c760eb0b35efd2e497f832dc
cloudghost <j86121100@gmail.com>
2018-06-02 18:30:52 +0800

    剩一些bug

--- app/src/main/java/com/example/momentary/sportapp/NotebookActivity.java
+++ app/src/main/java/com/example/momentary/sportapp/NotebookActivity.java
@@ -2,6 +2,7 @@ package com.example.momentary.sportapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.location.LocationManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -9,91 +10,94 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.database.sqlite.SQLiteDatabase;
 
-import com.google.android.gms.maps.MapView;
+import java.util.ArrayList;
 
 import static android.content.Context.LOCATION_SERVICE;
 import static android.content.Context.MODE_PRIVATE;
 
-public class NotebookActivity extends android.support.v4.app.Fragment {
-
-    private final static String createTitle = "CREATE TABLE tableFodder(_id interger not null,name text, count int, price int)";
-    private final static String createContent= "CREATE TABLE tableWallet(_id interger not null,money int)";
-
-    View v ;
-
-    private TextView show;
-    private EditText title;
-    private EditText content;
-    private Button btnadd;
-    private Button btndel;
-    private Button btnchange;
-    private Button btnsearch;
-
-    @SuppressLint("MissingPermission")
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v = inflater.inflate(R.layout.activity_notebook, container, false);
-
-        Spinner spinner=(Spinner) v.findViewById(R.id.spinner);
 
 
-        show =(TextView)v.findViewById(R.id.textView_show);
-        title =(EditText)v.findViewById(R.id.editText_title);
-        content =(EditText)v.findViewById(R.id.editText_content);
-        btnadd = (Button)v.findViewById(R.id.Add);
-        btnadd.setOnClickListener(rec_add);
-        btndel = (Button)v.findViewById(R.id.Delete);
-        btndel.setOnClickListener(rec_del);
-        btnchange = (Button)v.findViewById(R.id.Change);
-        btnchange.setOnClickListener(rec_change);
-        btnsearch = (Button)v.findViewById(R.id.Search);
-        btnsearch.setOnClickListener(rec_search);
 
-        String[] lunch = {"雞腿飯", "魯肉飯", "排骨飯", "水餃", "陽春麵"};
-        ArrayAdapter<String> lunchList = new ArrayAdapter<>(getActivity(),
-                android.R.layout.simple_spinner_dropdown_item,
-                lunch);
-        spinner.setAdapter(lunchList);
-        return v;
+public class NotebookActivity extends AppCompatActivity {
+
+    FileProcess fp;
+    boolean bSDCard = false; // true;
+    private Button btn_addnote;
+    private ListView lv_notes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notebook);
+
+        fp = new FileProcess(this, bSDCard);
+
+        btn_addnote = (Button) findViewById(R.id.btn_addnote);
+        btn_addnote.setOnClickListener(addnote);
+
+        lv_notes = (ListView) findViewById(R.id.lv_notes);
+        lv_notes.setOnItemClickListener(iclick);
+        lv_notes.setOnItemLongClickListener(ilclick);
     }
 
-    private View.OnClickListener rec_add = new View.OnClickListener(){
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ArrayList<String> notelist = fp.getNoteList();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, notelist);
+        lv_notes.setAdapter(adapter);
+    }
+
+    private View.OnClickListener addnote = new View.OnClickListener() {
         @Override
-        public void onClick(View view) {
-            String addtitle = title.getText().toString();
-            String addcontent = content.getText().toString();
-
-
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(NotebookActivity.this, NoteEditor.class);
+            intent.putExtra("SDCARD", bSDCard);
+            intent.putExtra("NOTEPOS", -1);
+            startActivity(intent);
         }
     };
 
-    private View.OnClickListener rec_del = new View.OnClickListener(){
+    AdapterView.OnItemClickListener iclick = new AdapterView.OnItemClickListener() {
+
         @Override
-        public void onClick(View view) {
-            String deltitle = title.getText().toString();
+        public void onItemClick(AdapterView<?> av, View v,
+                                int position, long id) {
+
+            Intent intent = new Intent();
+            intent.setClass(NotebookActivity.this, NoteEditor.class);
+            intent.putExtra("SDCARD", bSDCard);
+            intent.putExtra("NOTEPOS", position);
+            startActivity(intent);
         }
     };
 
-    private View.OnClickListener rec_change = new View.OnClickListener(){
-        @Override
-        public void onClick(View view) {
-            String changetitle = title.getText().toString();
-            String changecontent = title.getText().toString();
+    AdapterView.OnItemLongClickListener ilclick = new AdapterView.OnItemLongClickListener() {
 
+        @Override
+        public boolean onItemLongClick(AdapterView<?> av, View v,
+                                       int position, long id) {
+
+            fp.delNote(position);
+            ArrayList<String> notelist = fp.getNoteList();
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                    (NotebookActivity.this, android.R.layout.simple_list_item_1, notelist);
+            lv_notes.setAdapter(adapter);
+            return false;
         }
     };
-
-    private View.OnClickListener rec_search = new View.OnClickListener(){
-        @Override
-        public void onClick(View view) {
-            String searchtitle = title.getText().toString();
-        }
-    };
-
 }
